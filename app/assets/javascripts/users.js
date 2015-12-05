7216d978debe1f321d4e8628ab1aaf83512094f3
Michael Ballard <genieapp7@gmail.com>
2015-12-05 22:32:35 +0000

    added stripe js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -28,6 +28,6 @@ $(document).ready(function() {
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
     // Submit the form:
-    f.get(0).submit(); 
+    f.get(0).submit();
   }
 });
